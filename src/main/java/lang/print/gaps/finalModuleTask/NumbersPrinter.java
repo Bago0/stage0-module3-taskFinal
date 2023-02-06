f57c3e63cd9e0068b9242b5f55ab09018b6e6408
Bago0 <jgamadzebagrati@gmail.com>
2023-02-06 23:51:51 +0400

    exercise 8 update(1)

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -2,11 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-        if(number > 0){
-            System.out.println(true);
-        }else{
-            System.out.println(false);
-        }
+       boolean result = number > 0;
+       System.out.println(result);
 
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,7 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
-        float formula = (float) (temperatureCelsius * 33.8);
+        float formula = (float) (temperatureCelsius * 1.8 + 32);
         System.out.println(formula);
     }
 }
